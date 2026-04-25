26452e957217188262d12ded171313adcb1d97a4
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-04-25 20:53:53 +0300

    fix: declare WebClient.builder bean

--- src/main/java/com/iqkv/foundation/gatewayservice/infrastructure/config/WebClientConfig.java
+++ src/main/java/com/iqkv/foundation/gatewayservice/infrastructure/config/WebClientConfig.java
@@ -17,8 +17,10 @@
 package com.iqkv.foundation.gatewayservice.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfig {
   @Bean
   public WebClient.Builder webClientBuilder() {
